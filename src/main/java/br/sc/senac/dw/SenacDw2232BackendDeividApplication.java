e6f944c623e7d45780303403cc2ec9538ea2c445
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2023-08-15 11:12:54 -0300

    Initial Commit
    Aprendendo Spring Boot na sofrência.

---
+++ src/main/java/br/sc/senac/dw/SenacDw2232BackendDeividApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacDw2232BackendDeividApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacDw2232BackendDeividApplication.class, args);
+	}
+
+}
